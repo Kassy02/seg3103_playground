b0e109c628d57d41f2806e9e2d50289f3260ff08
Kassy02 <gfant077@uottawa.ca>
2024-06-13 15:11:45 -0400

    Update math_operations.py

--- lab04/Test-driven/src/math_operations.py
+++ lab04/Test-driven/src/math_operations.py
@@ -4,3 +4,11 @@ def add(a, b):
 
 def subtract(a, b):
     return a - b
+# src/math_operations.py
+def add(a, b):
+    # Example of a trivial refactor
+    return a + b
+
+def subtract(a, b):
+    # Example of a trivial refactor
+    return a - b
