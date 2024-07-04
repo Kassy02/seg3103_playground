236e4b7a902cda6517f9267f889c5c0ac9b3f462
Kassy02 <gfant077@uottawa.ca>
2024-07-04 13:25:29 -0400

    Extract helper methods grade_to_letter and grade_to_numeric

--- Assigment2/grades/grades/lib/grades/calculator.ex
+++ Assigment2/grades/grades/lib/grades/calculator.ex
@@ -16,6 +16,38 @@ defmodule Grades.Calculator do
     0.2 * avg_labs + 0.3 * avg_homework + 0.2 * midterm + 0.3 * final
   end
 
+  defp grade_to_letter(mark) do
+    cond do
+      mark > 0.895 -> "A+"
+      mark > 0.845 -> "A"
+      mark > 0.795 -> "A-"
+      mark > 0.745 -> "B+"
+      mark > 0.695 -> "B"
+      mark > 0.645 -> "C+"
+      mark > 0.595 -> "C"
+      mark > 0.545 -> "D+"
+      mark > 0.495 -> "D"
+      mark > 0.395 -> "E"
+      :else -> "F"
+    end
+  end
+
+  defp grade_to_numeric(mark) do
+    cond do
+      mark > 0.895 -> 10
+      mark > 0.845 -> 9
+      mark > 0.795 -> 8
+      mark > 0.745 -> 7
+      mark > 0.695 -> 6
+      mark > 0.645 -> 5
+      mark > 0.595 -> 4
+      mark > 0.545 -> 3
+      mark > 0.495 -> 2
+      mark > 0.395 -> 1
+      :else -> 0
+    end
+  end
+
   def percentage_grade(%{homework: homework, labs: labs, midterm: midterm, final: final}) do
     avg_homework = avg(homework)
     avg_labs = avg(labs)
@@ -32,20 +64,7 @@ defmodule Grades.Calculator do
       "EIN"
     else
       mark = calculate_grade(avg_labs, avg_homework, midterm, final)
-
-      cond do
-        mark > 0.895 -> "A+"
-        mark > 0.845 -> "A"
-        mark > 0.795 -> "A-"
-        mark > 0.745 -> "B+"
-        mark > 0.695 -> "B"
-        mark > 0.645 -> "C+"
-        mark > 0.595 -> "C"
-        mark > 0.545 -> "D+"
-        mark > 0.495 -> "D"
-        mark > 0.395 -> "E"
-        :else -> "F"
-      end
+      grade_to_letter(mark)
     end
   end
 
@@ -58,20 +77,7 @@ defmodule Grades.Calculator do
       0
     else
       mark = calculate_grade(avg_labs, avg_homework, midterm, final)
-
-      cond do
-        mark > 0.895 -> 10
-        mark > 0.845 -> 9
-        mark > 0.795 -> 8
-        mark > 0.745 -> 7
-        mark > 0.695 -> 6
-        mark > 0.645 -> 5
-        mark > 0.595 -> 4
-        mark > 0.545 -> 3
-        mark > 0.495 -> 2
-        mark > 0.395 -> 1
-        :else -> 0
-      end
+      grade_to_numeric(mark)
     end
   end
 end
